58b0e69e94241173fcccaa88508932ead373bf80
spidey <satyamkumar5254@gmail.com>
2021-03-24 07:16:17 +0530

    add monitor choose option and some refactoring of code

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
 const express = require('express')
 const cors = require('cors')
-
-const { exec } = require('child_process')
 const fs = require('fs')
 const { default: axios } = require('axios')
+const { setBrightness, getMonitors } = require('./utils')
 
 const app = express()
-app.use(express.static(__dirname + '/public'))
 app.use(cors())
+app.use(express.static(__dirname + '/public'))
 
 setInterval(async () => {
   await axios.get('http://localhost:4000/get')
 }, 3000)
 
 
-app.get('/get', (req, res) => {
+app.get('/get', async (req, res) => {
   const value = fs.readFileSync(__dirname + '/value.json')
   const { brightness } = JSON.parse(value.toString())
-
-
-  exec('xrandr --verbose --current | grep HDMI-0 -A5 | tail -n1', (err, stdout, stderr) => {
-    if (!err) {
-      const already = stdout.trim().split(' ')[1]
-      if (already !== brightness)
-        exec(`xrandr --output HDMI-0 --brightness ${brightness}`)
-    }
-  })
-
-
+  const monitor = await getMonitors()
+  await setBrightness(monitor[0], brightness)
   res.send(brightness)
 })
 
-app.get('/set/:value', (req, res) => {
-  exec(`xrandr --output HDMI-0 --brightness ${req.params.value}`)
-  fs.writeFileSync(__dirname + '/value.json', JSON.stringify({ brightness: req.params.value }))
+app.get('/set/:value', async (req, res) => {
+  const monitor = await getMonitors()
+  await setBrightness(monitor[0], req.params.value)
   res.send('done')
 })
 
---
+++ utils.js
@@ -0,0 +1,38 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+
+const getMonitors = () => {
+  return new Promise((resolve, reject) => {
+    exec('xrandr | grep " connected" | cut -f1 -d " "', (err, stdout) => {
+      if (err) reject(err)
+      resolve(stdout.trim().split(' '))
+    })
+  })
+}
+const getCurrentBrightness = (monitorName) => {
+  return new Promise((resolve, reject) => {
+    exec(`xrandr --verbose --current | grep ${monitorName} -A5 | tail -n1`, (err, stdout) => {
+      if (err) reject(err)
+      resolve(stdout.trim().split(' ')[1])
+    })
+  })
+}
+
+const setBrightness = async (monitorName, brightness) => {
+  const current = await getCurrentBrightness(monitorName)
+  return new Promise((resolve, reject) => {
+    if (current * 100 === brightness * 100) {
+      console.log('here')
+      return resolve('done')
+    }
+
+    exec(`xrandr --output ${monitorName} --brightness ${brightness}`, (err, stdout) => {
+      console.log('there')
+      if (err) return reject(`something went wrong. \n ${err}`)
+      fs.writeFileSync(__dirname + '/value.json', JSON.stringify({ brightness }))
+      return resolve(stdout.trim())
+    })
+  })
+}
+
+module.exports = { getMonitors, setBrightness, getCurrentBrightness }
